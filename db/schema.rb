f8394d76848c44b93890b75efaecfb2d43a8bf60
YoshikiIkegaya <mr.ikegaya@gmail.com>
2016-07-17 15:16:21 +0900

    modified for windows

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160630042527) do
 
-  create_table "events", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "note_id"
