1bdc6a7260c6fdfe68688f7dbc76d3bdbaa4b483
YoshikiIkegaya <ikegaya@kemuri.in>
2016-07-18 22:43:56 +0530

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718072609) do
+ActiveRecord::Schema.define(version: 20160718170122) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 20160718072609) do
     t.date     "date"
     t.string   "place"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.integer  "admin_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
