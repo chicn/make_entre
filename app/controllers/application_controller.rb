9e96f57725c98887a786abcdcd42b6908971b1b0
YoshikiIkegaya <ikegaya@kemuri.in>
2016-07-19 22:57:23 +0530

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def authenticate_admin_user!
+  authenticate_user!
+
+    unless current_user.role?
+      flash[:alert] = "This area is restricted to administrators only."
+      redirect_to root_path
+    end
+  end
+
   def after_sign_in_path_for(resource)
       events_path
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,2 @@
 class Event < ActiveRecord::Base
-    belongs_to :admin_user
 end
