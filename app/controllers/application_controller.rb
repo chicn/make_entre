d66c47a7b5f01a9410f9aac3e92f401b4fe1d56b
YoshikiIkegaya <ikegaya@kemuri.in>
2016-07-20 22:55:49 +0530

    modified

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    case resource
+    when user
       events_path
+    when admin_user
+      new_admin_user_session_path
+    end
   end
 
   def after_sign_out_path_for(resource)
