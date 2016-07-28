0b7996070515229100b5379cfb8db63bb6d796bb
YoshikiIkegaya <ikegaya@kemuri.in>
2016-07-28 21:46:54 +0530

    fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :validatable
+         :validatable, :confirmable
   has_many :notes
   has_many :likes
   has_many :like_notes, through: :likes, source: :note
